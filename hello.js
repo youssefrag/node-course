8a4cc8479a8d80eb97360c472787ab7c7477f140
Youssef Ragab <ragab.youssef95@gmail.com>
2023-03-19 12:08:59 +0200

    process argv

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
-function findMeaning() {
-  return 42;
-}
+const mission = process.argv[2];
 
-console.log(findMeaning());
+if (mission === "learn") {
+  console.log("Time to write some Node code!");
+} else {
+  console.log(`Is ${mission} really more fun?`);
+}
