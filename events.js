e45099f16b0873060f9eb714c782e564b8915cb7
Youssef Ragab <ragab.youssef95@gmail.com>
2023-05-16 17:43:44 -0400

    exit process

--- events.js
+++ events.js
@@ -11,4 +11,10 @@ celebrity.on("race win", () => {
   console.log("Boo I could have done better than that!");
 });
 
+process.on("exit", (code) => {
+  console.log("Process exit event with code: ", code);
+});
+
+celebrity.emit("race win");
+celebrity.emit("race lost");
 celebrity.emit("race win");
