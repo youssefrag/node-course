f73e27a4a5b49216683d73e35ae40ad89689ddf6
Youssef Ragab <ragab.youssef95@gmail.com>
2023-05-16 17:57:03 -0400

    add event conditionals

--- events.js
+++ events.js
@@ -2,19 +2,22 @@ const EventEmitter = require("events");
 const celebrity = new EventEmitter();
 
 // Sunscribe to the celenrity for Observer 1
-celebrity.on("race win", () => {
-  console.log("Congratulations! You are the best!");
+celebrity.on("race", (result) => {
+  if (result === "win") {
+    console.log("Congratulations! You are the best!");
+  }
 });
 
 // Sunscribe to the celenrity for Observer 2
-celebrity.on("race win", () => {
-  console.log("Boo I could have done better than that!");
+celebrity.on("race", (result) => {
+  if (result === "win") {
+    console.log("Boo I could have done better than that!");
+  }
 });
 
 process.on("exit", (code) => {
   console.log("Process exit event with code: ", code);
 });
 
-celebrity.emit("race win");
-celebrity.emit("race lost");
-celebrity.emit("race win");
+celebrity.emit("race", "win");
+celebrity.emit("race", "lost");
