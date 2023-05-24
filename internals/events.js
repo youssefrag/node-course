fa647a497de05ce9e2e2e8d19185a9ec8a261390
Youssef Ragab <ragab.youssef95@gmail.com>
2023-05-24 09:25:08 -0400

    complete internals

--- internals/events.js
+++ internals/events.js
@@ -1,18 +1,24 @@
 const EventEmitter = require("events");
 const celebrity = new EventEmitter();
 
-// Sunscribe to the celenrity for Observer 1
+const spectator = new EventEmitter();
+
+// Sunscribe to the celebrity for Observer 1
 celebrity.on("race", (result) => {
   if (result === "win") {
     console.log("Congratulations! You are the best!");
   }
 });
 
-// Sunscribe to the celenrity for Observer 2
+// Sunscribe to the celebrity for Observer 2
 celebrity.on("race", (result) => {
   if (result === "win") {
     console.log("Boo I could have done better than that!");
   }
+});
+
+spectator.on("finish", () => {
+  console.log("Good job, you were fast");
 });
 
 process.on("exit", (code) => {
@@ -20,4 +26,4 @@ process.on("exit", (code) => {
 });
 
 celebrity.emit("race", "win");
-celebrity.emit("race", "lost");
+spectator.emit("finish");
