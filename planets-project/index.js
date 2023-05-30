e75096f68c4a7ff8757d4691d1077521a4253d9b
Youssef Ragab <ragab.youssef95@gmail.com>
2023-05-30 14:50:33 -0400

    read stream

--- planets-project/index.js
+++ planets-project/index.js
@@ -1,3 +1,18 @@
 const { parse } = require("csv-parse");
+const fs = require("fs");
 
-console.log(parse);
+const results = [];
+
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("done");
+  });
+
+// parse();
