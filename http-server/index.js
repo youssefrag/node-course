ce855d0b18ce31f638f3ad53fae5b4e51e797fea
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-02 15:40:14 -0400

    parameterized urls

--- http-server/index.js
+++ http-server/index.js
@@ -2,11 +2,27 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/friends") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ id: 1, name: "Sir Isaac Newton" }));
-  } else if (req.url === "/messages") {
+const server = http.createServer();
+
+const friends = [
+  { id: 0, name: "Nikola Tesla" },
+  { id: 1, name: "Sir Isaac Newton" },
+  { id: 2, name: "Albert Einstein" },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+  if (items[1] === "friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
     res.write("<ul>");
@@ -15,6 +31,9 @@ const server = http.createServer((req, res) => {
     res.write("</ul>");
     res.write("</body>");
     res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
     res.end();
   }
 });
