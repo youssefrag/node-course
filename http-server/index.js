15e04882d3a4e7954670ac3462670b30978448c1
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-02 14:33:43 -0400

    html response

--- http-server/index.js
+++ http-server/index.js
@@ -3,8 +3,20 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ id: 1, name: "Sir Isaac Newton" }));
+  if (req.url === "/friends") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ id: 1, name: "Sir Isaac Newton" }));
+  } else if (req.url === "/messages") {
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>What are your thought on astronomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
