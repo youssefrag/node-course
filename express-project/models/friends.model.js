c464d8d638e82b8e46c0d709e954e6dd0dd31f64
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-12 16:33:04 -0400

    mcv layout

---
+++ express-project/models/friends.model.js
@@ -0,0 +1,6 @@
+const friends = [
+  { id: 0, name: "Albert Einstein" },
+  { id: 1, name: "Sir Isaac Newton" },
+];
+
+module.exports = friends;
--- express-project/server.js
+++ express-project/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
+const friendsController = require("./controllers/friends.controller");
+
+const messagesController = require("./controllers/messages.controller");
+
 const app = express();
 
 const PORT = 3000;
-
-const friends = [
-  { id: 0, name: "Albert Einstein" },
-  { id: 1, name: "Sir Isaac Newton" },
-];
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -18,36 +17,12 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post("/friends", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({ error: "Missing friend name" });
-  }
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  };
-  friends.push(newFriend);
+app.post("/friends", friendsController.postFriend);
+app.get("/friends", friendsController.getFriends);
+app.get("/friends/:friendId", friendsController.getFriend);
 
-  res.json(newFriend);
-});
-
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
-
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({ error: "Friend does not exist" });
-  }
-});
-
-app.get("/messages");
-
-app.post("/messages");
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessage);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
