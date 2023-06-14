c87872259e215e7b5a15967fa881db00e4ce68d7
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-13 20:29:50 -0400

    template engine

--- express-project/server.js
+++ express-project/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
 
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 const PORT = 3000;
 
@@ -14,8 +18,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends Are VERY Clever",
+    caption: "Let's go skiing!",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
