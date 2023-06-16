8c3aff226037b903b93860a5c3773c1098979b8e
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-16 16:15:53 -0400

    test post request

--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -11,7 +11,54 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launches", () => {
-  test("It should respond with 200 success", () => {});
-  test("It should catch missing required properties", () => {});
-  test("It should catch invalid dates", () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing required properties", async () => {
+    // const response = await request(app)
+    //   .post("/launches")
+    //   .send({
+    //     mission: "USS Enterprise",
+    //     rocket: "NCC 1701-D",
+    //     launchDate: "January 4, 2028",
+    //   })
+    //   .expect("Content-Type", /json/)
+    //   .expect(400);
+  });
+  test("It should catch invalid dates", async () => {
+    // const response = await request(app)
+    //   .post("/launches")
+    //   .send({
+    //     mission: "USS Enterprise",
+    //     rocket: "NCC 1701-D",
+    //     target: "Kepler-186 f",
+    //     launchDate: "hello",
+    //   })
+    //   .expect("Content-Type", /json/)
+    //   .expect(400);
+  });
 });
