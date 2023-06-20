a27152c1d2a4a8e4de1584c683ef91ec420bfba6
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-20 15:26:52 -0400

    connect nasa to mongo

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:O29XQ3dPYrH3Q5Jg@cluster0.u8rse9b.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
