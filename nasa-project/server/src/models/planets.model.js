c57e1b77e82a115d3a14da03e3b1b4ed97e8c16f
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-21 12:04:59 -0400

    get all planets function

--- nasa-project/server/src/models/planets.model.js
+++ nasa-project/server/src/models/planets.model.js
@@ -44,8 +44,8 @@ function loadPlanetsData() {
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
 }
 
 module.exports = { loadPlanetsData, getAllPlanets };
