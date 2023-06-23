0c8ba2188739507fd74097033e527b68b88a8442
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-22 20:23:44 -0400

    securing password with dotenv

--- nasa-project/server/src/services/mongo.js
+++ nasa-project/server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:O29XQ3dPYrH3Q5Jg@cluster0.u8rse9b.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
