855ae291a272b45044ad34eaaae382bc861d644e
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-28 14:14:11 -0400

    implement logot

--- security-example/server.js
+++ security-example/server.js
@@ -83,7 +83,10 @@ app.get(
   }
 );
 
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout(); //Removes req.user and clears any logged in session
+  return res.redirect("/");
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
