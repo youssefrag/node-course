466ca3327cf6c6f30e07962e2b6bf3fef27a41cf
Youssef Ragab <ragab.youssef95@gmail.com>
2023-06-28 12:32:58 -0400

    endoint restriction

--- security-example/server.js
+++ security-example/server.js
@@ -59,8 +59,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  //req.user
-  const isLoggedIn = true; //TODO
+  console.log("Current user is:", req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in!",
