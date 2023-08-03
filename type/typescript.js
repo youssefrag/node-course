d3ac551c6bea96e372ce287921ddf045fe12cc95
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-03 10:36:18 -0400

    basic typing

---
+++ type/typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var answer = sum("hello", 5);
+console.log(answer);
---
+++ type/typescript.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+const answer = sum("hello", 5);
+
+console.log(answer);
